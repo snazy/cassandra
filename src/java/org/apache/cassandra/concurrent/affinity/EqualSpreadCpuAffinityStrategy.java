c14d451d0da2caa1dbf7791e360dff105e464dce
Jason Brown <jasedbrown@gmail.com>
2013-11-13 06:38:53 -0800

    adding ability to ban cpus from ESCAS

--- src/java/org/apache/cassandra/concurrent/affinity/EqualSpreadCpuAffinityStrategy.java
+++ src/java/org/apache/cassandra/concurrent/affinity/EqualSpreadCpuAffinityStrategy.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import com.higherfrequencytrading.affinity.AffinityLock;
 import com.higherfrequencytrading.affinity.AffinitySupport;
 import com.higherfrequencytrading.affinity.CpuLayout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Intended to be used with an AbstractExecutorService (ThreadPoolExecutor or ForkJoinPool).
@@ -15,9 +17,42 @@ import com.higherfrequencytrading.affinity.CpuLayout;
  */
 public class EqualSpreadCpuAffinityStrategy implements CpuAffinityStrategy
 {
+    private static final Logger logger = LoggerFactory.getLogger(EqualSpreadCpuAffinityStrategy.class);
     private static final CpuLayout CPU_LAYOUT = AffinityLock.cpuLayout();
 
+    private static final int[] bannedCpus = initBannedCpus();
+
     private final AtomicInteger cpuAffinityOffset;
+
+    /**
+     * Allow users to ban CPUs from usage (only when using this strategy). The thought is that the kernel (or admins)
+     * will often assign a certain cpu to handle hardware interrupts, and we don't want to pin a thread to the same
+     * cpu that's handling all the network interrupts and so on.
+     */
+    private static int[] initBannedCpus()
+    {
+        //TODO: is there something better than a jvm property?
+        final String bannedCpusProp = System.getProperty("threadAff.bannedCpus");
+        if(bannedCpusProp == null)
+            return new int[0];
+
+        final String[] cpus = bannedCpusProp.split(",");
+        int[] bannedCpus = new int[cpus.length];
+
+        if (bannedCpus.length == CPU_LAYOUT.cpus())
+        {
+            logger.error("You have chosen to ban cpus {}, which is the same count as number of CPUs in your machine: {}. "
+                    + "Ignoring cpu banning.", bannedCpus.length, CPU_LAYOUT.cpus());
+            return new int[0];
+        }
+
+        int idx = 0;
+        for (String cpu : cpus)
+        {
+            bannedCpus[idx] = Integer.parseInt(cpu.trim());
+        }
+        return bannedCpus;
+    }
 
     public EqualSpreadCpuAffinityStrategy()
     {
@@ -27,8 +62,19 @@ public class EqualSpreadCpuAffinityStrategy implements CpuAffinityStrategy
 
     public void setCpuAffinity(Thread t)
     {
-        final int offset = cpuAffinityOffset.getAndIncrement();
-        final long cpuId = offset % CPU_LAYOUT.cpus();
+        long cpuId;
+        outer: while (true)
+        {
+            final int offset = cpuAffinityOffset.getAndIncrement();
+            cpuId = offset % CPU_LAYOUT.cpus();
+            for (int i = 0; i < bannedCpus.length; i++)
+            {
+                if(cpuId == bannedCpus[i])
+                    continue outer;
+            }
+            break;
+        }
+
         final long mask = 1 << cpuId;
         AffinitySupport.setAffinity(mask);
     }
