eeb82440ede02e9df8010020021fc1c741d921b2
Jason Brown <jasedbrown@gmail.com>
2015-04-27 16:21:01 -0700

    Revert "introduce concurrency to search/range scans"
    This *partially* reverts commit 5642640ccef603a8b8b512d68d4cf755ccb1671e.
    Just removes the concurrency stuff from SASI

--- src/java/org/apache/cassandra/db/index/search/SuffixIterator.java
+++ src/java/org/apache/cassandra/db/index/search/SuffixIterator.java
@@ -1,112 +1,54 @@
 package org.apache.cassandra.db.index.search;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.cassandra.config.DatabaseDescriptor;
+import com.google.common.collect.AbstractIterator;
 import org.apache.cassandra.db.index.search.container.TokenTree.Token;
 import org.apache.cassandra.db.index.search.plan.Expression;
 import org.apache.cassandra.db.index.utils.LazyMergeSortIterator;
 import org.apache.cassandra.db.index.utils.SkippableIterator;
 import org.apache.cassandra.io.util.FileUtils;
 
-import com.google.common.collect.AbstractIterator;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.Uninterruptibles;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SuffixIterator extends AbstractIterator<Token> implements SkippableIterator<Long, Token>
 {
-    private static final Logger logger = LoggerFactory.getLogger(SuffixIterator.class);
-
-    private static final ThreadLocal<ExecutorService> SEARCH_EXECUTOR = new ThreadLocal<ExecutorService>()
-    {
-        public ExecutorService initialValue()
-        {
-            final String currentThread = Thread.currentThread().getName();
-            final int concurrencyFactor = DatabaseDescriptor.searchConcurrencyFactor();
-
-            logger.info("Search Concurrency Factor is set to {} for {}", concurrencyFactor, currentThread);
-
-            return (concurrencyFactor <= 1)
-                    ? MoreExecutors.sameThreadExecutor()
-                    : Executors.newFixedThreadPool(concurrencyFactor, new ThreadFactory()
-                      {
-                          public final AtomicInteger count = new AtomicInteger();
-
-                          @Override
-                          public Thread newThread(Runnable task)
-                          {
-                              Thread newThread = new Thread(task, currentThread + "-SEARCH-" + count.incrementAndGet());
-                              newThread.setDaemon(true);
-
-                              return newThread;
-                          }
-                      });
-        }
-    };
-
     private final SkippableIterator<Long, Token> union;
-    private final List<SSTableIndex> referencedIndexes = new CopyOnWriteArrayList<>();
+    private final List<SSTableIndex> referencedIndexes = new ArrayList<>();
     private final long count;
 
-    public SuffixIterator(final Expression expression,
+    public SuffixIterator(Expression expression,
                           SkippableIterator<Long, Token> memtableIterator,
                           final Collection<SSTableIndex> perSSTableIndexes)
     {
-        final AtomicLong tokenCount = new AtomicLong(0);
-        final CountDownLatch latch = new CountDownLatch(perSSTableIndexes.size());
-        final List<SkippableIterator<Long, Token>> keys = new CopyOnWriteArrayList<>();
+        List<SkippableIterator<Long, Token>> keys = new ArrayList<>(perSSTableIndexes.size());
 
+        long tokenCount = 0;
         if (memtableIterator != null)
         {
             keys.add(memtableIterator);
-            tokenCount.getAndAdd(memtableIterator.getCount());
+            tokenCount += memtableIterator.getCount();
         }
 
-        for (final SSTableIndex index : perSSTableIndexes)
+        for (SSTableIndex index : perSSTableIndexes)
         {
             if (!index.reference())
+                continue;
+
+            SkippableIterator<Long, Token> results = index.search(expression);
+            if (results == null)
             {
-                latch.countDown();
+                index.release();
                 continue;
             }
 
-            SEARCH_EXECUTOR.get().submit(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    try
-                    {
-                        SkippableIterator<Long, Token> results = index.search(expression);
-                        if (results == null)
-                        {
-                            index.release();
-                            return;
-                        }
-
-                        keys.add(results);
-                        referencedIndexes.add(index);
-                        tokenCount.getAndAdd(results.getCount());
-                    }
-                    finally
-                    {
-                        latch.countDown();
-                    }
-                }
-            });
+            keys.add(results);
+            referencedIndexes.add(index);
+            tokenCount += results.getCount();
         }
 
-        Uninterruptibles.awaitUninterruptibly(latch);
-
         union = new LazyMergeSortIterator<>(LazyMergeSortIterator.OperationType.OR, keys);
-        count = tokenCount.get();
+        count = tokenCount;
     }
 
     @Override
